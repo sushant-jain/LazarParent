0b3b58f351dbad2b0d167a6388b80a3aca090d20
Ayush Aggarwal <lookforayush@gmail.com>
2018-03-29 22:54:30 +0530

    SignUp Activity

--- app/src/main/java/com/trufle/lazarparent/MainActivity.java
+++ app/src/main/java/com/trufle/lazarparent/MainActivity.java
@@ -1,11 +1,13 @@
 package com.trufle.lazarparent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,19 +17,33 @@ public class MainActivity extends AppCompatActivity {
     EditText mid, pass;
     AppCompatButton login;
     TextView link_signup;
+    LinearLayout mainlayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mid=findViewById(R.id.MID);
+        findViewById(R.id.mainlayout).requestFocus();
         pass=findViewById(R.id.pass);
         login=findViewById(R.id.login);
         link_signup=findViewById(R.id.link_signup);
+        mid.setActivated(false);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"lalala",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        link_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MainActivity.this, "Random Random", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, Signup.class);
+
+                startActivity(intent);
+                finish();
             }
         });
     }
---
+++ app/src/main/java/com/trufle/lazarparent/Signup.java
@@ -0,0 +1,37 @@
+package com.trufle.lazarparent;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Signup extends AppCompatActivity {
+    EditText mid, aadhar, pass;
+    AppCompatButton signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        mid=findViewById(R.id.MID);
+        aadhar=findViewById(R.id.Aadhar);
+        pass=findViewById(R.id.pass);
+        signup=findViewById(R.id.signup);
+        mid.clearFocus();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Signup.this, "Signup Bencho", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+}
