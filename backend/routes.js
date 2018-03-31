cf772b806932feede376d29539dce536150dfcf1
Nikhil Kumar Mishra <nkmishra1997@gmail.com>
2018-03-31 11:56:00 +0530

    call, sms, email as endpoint

--- backend/routes.js
+++ backend/routes.js
@@ -5,6 +5,7 @@ PDFDocument = require('pdfkit');
 blobStream  = require('blob-stream')
 fs = require('fs')
 var route = express.Router();
+var client = require('twilio')('ACe7cdf1cec256c369dc641220aa082d4f','02c059879ff5931df43865448d37dc00');
 
 
 // Defining a route that binds the GET method
@@ -48,7 +49,7 @@ route.get('/genPDF', function(req, res) {
 	var options = {
 		method: 'GET',
 		url: 'https://parseapi.back4app.com/classes/Mother',
-		qs: { where: '{"LocationID":"cde"}' },
+		qs: { where: '{"LocationID":"Chj1202"}' },
 		headers:
 		{'x-parse-rest-api-key': 'ovpPF8ytV4BicURhTrUScFawAuCoG0SZIKtdG1p1',
 		'x-parse-application-id': 'tdmZYblELna1qEfzXCbWVPkLT6meo8rGBeS6hqFe' }
@@ -77,5 +78,93 @@ route.get('/genPDF', function(req, res) {
 	});
 });
 
+route.get('/call', function(req, res) {
+	client.calls.create({
+		url: 'http://k003.kiwi6.com/hotlink/tmvhcviq4u/voice.xml',
+		to: '+918368706829',
+		from: '+12052360143',
+	  })
+	  .then(call => process.stdout.write(call.sid));
+})
 
+route.get('/message', function(req, res) {
+	client.messages.create({
+		// you can use request.params to pass the number you want to send
+		to:'+918368706829', // Any number Twilio can deliver to
+		from: '+12052360143', // A number you bought from Twilio and can use for outbound communication
+		body: 'टीकाकरण की अगली तारीख 31-3-2018 है' // body of the SMS message
+
+		}, {
+		function(error, responseData) {
+		  if (error) {
+			 console.log(error);
+			 res.send(error);
+		  } else {
+			 console.log(responseData);
+			 res.send(responseData);
+		  }
+		}
+	   });
+})
+
+route.get('/email', function(request, response) {
+	var sg = require('sendgrid')('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');
+
+  // Create the SendGrid Request
+  var reqSG = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: {
+      personalizations: [
+        {
+          to: [
+            {
+              // This field is the "to" in the email
+              email: 'nkmishra1997@gmail.com',
+            },
+          ],
+          // This field is the "subject" in the email
+          subject: 'Test E-mail',
+        },
+      ],
+      // This field contains the "from" information
+      from: {
+        email: 'info@lazar.com',
+        name: 'Lazar Reminder',
+      },
+      // This contains info about the "reply-to"
+      // Note that the "reply-to" may be different than the "from"
+      reply_to: {
+        email: 'info@lazar.com',
+        name: 'Team Lazar',
+      },
+      content: [
+        {
+          // You may want to leave this in text/plain,
+          // Although some email providers may accept text/html
+          type: 'text/plain',
+          // This field is the body of the email
+          value: 'टीकाकरण की अगली तारीख 31-3-2018 है',
+        },
+      ],
+    },
+  });
+
+  // Make a SendGrid API Call
+  sg.API(reqSG, function(SGerror, SGresponse) {
+    // Testing if some error occurred
+    if (SGerror) {
+      // Ops, something went wrong
+      console.error('Error response received');
+      console.error('StatusCode=' + SGresponse.statusCode);
+      console.error(JSON.stringify(SGresponse.body));
+      response.send('Error = ' + JSON.stringify(SGresponse.body));
+    }
+    else {
+      // Everything went fine
+      console.log('Email sent!');
+      response.send('Email sent!');
+    }
+  });
+})
 module.exports = route;
